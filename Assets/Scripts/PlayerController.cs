39046c84887aafbd25b245fc4aa097287169291a
ChristopherGug <cgugelmeier@gmail.com>
2020-12-08 21:46:30 -0500

    Created an overworld and a moveable character, started on animations

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public Rigidbody2D rb;
+    public Vector2 moveVector;
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        //if (Input.GetAxis("Horizontal") > 0)
+        //{
+        //    anim.SetFloat("Horizontal", 1);
+        //}
+        anim.SetFloat("Horizontal", Input.GetAxisRaw("Horizontal"));
+        anim.SetFloat("Vertical", Input.GetAxisRaw("Vertical"));
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        moveVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        transform.Translate(moveVector * moveSpeed);
+    }
+}
